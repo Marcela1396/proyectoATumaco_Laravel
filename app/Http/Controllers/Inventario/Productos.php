fc26ae8200bf4bef645d523081e42c2eff7d83ae
Marcela Guerrero <marcelaguerrero1396@gmail.com>
2020-12-02 12:14:08 -0500

    Clase 2-12-20

--- app/Http/Controllers/Inventario/Productos.php
+++ app/Http/Controllers/Inventario/Productos.php
@@ -3,12 +3,70 @@
 namespace App\Http\Controllers\Inventario;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class Productos extends Controller
 {
     public function index(){
-        return view('productos.listado');
+        // Consultar de productos
+        /*
+        $productos = DB::table('productos')
+                    ->select('nombreProducto', 'cantidadProducto', 'precioProducto','fotoProducto', 'categoria')
+                    ->get();
+        return view('productos.listado',['productos' => $productos] );
+        */
+        
+        /*Metodo First
+        $producto = DB::table('productos')->first();
+        return view('productos.listado',['p' => $producto] );
+        */
+
+        //Clausula where : orwhere
+        /*
+        $productos = DB::table('productos')
+                    ->select('nombreProducto', 'cantidadProducto', 'precioProducto','fotoProducto', 'categoria')
+                    ->where('categoria', '=', 1)
+                    ->where('precioProducto', '>=', 4000)
+                    ->orwhere('cantidadProducto', '>', 10)
+                    ->get();
+        return view('productos.listado',['productos' => $productos] );
+        */
+
+        // Clausula Value
+        /*
+        $producto = DB::table('productos')
+                    ->where('categoria', '=', 1)
+                    ->value('id');
+        return view('productos.listado',['p' => $producto] );
+        */
+
+        //Orderby
+        /*
+        $productos = DB::table('productos')
+                    ->select('nombreProducto', 'cantidadProducto', 'precioProducto','fotoProducto', 'categoria')
+                    ->orderBy('nombreProducto', 'asc')
+                    ->get();
+        return view('productos.listado',['productos' => $productos] );
+        */
+
+        //Clausula Skip/Take y Offset/Limit
+        /*
+        $productos = DB::table('productos')
+                    ->select('nombreProducto', 'cantidadProducto', 'precioProducto','fotoProducto', 'categoria')
+                    ->orderBy('nombreProducto', 'asc')
+                    ->skip('2')
+                    ->take('4')
+                    ->get();
+        return view('productos.listado',['productos' => $productos] );
+        */
+        // Uso de Join
+        $productos = DB::table('productos')
+                    ->join('categorias', 'categoria', '=', 'categorias.id')
+                    ->get();
+        return view('productos.listado',['productos' => $productos] );
+
+
     }
 
     public function ofertas(){
--- resources/views/productos/listado.blade.php
+++ resources/views/productos/listado.blade.php
@@ -1,4 +1,25 @@
 @extends('main')
 @section('contenido')
 <h1> Listado de productos </h1>
+<div class="container">
+    <div class ="row">
+    @foreach($productos as $p)
+        <div class="col-md-4">
+            <div class="card" style="width: 15rem;">
+                <img src='{{url("/imagenes/productos/$p->fotoProducto")}}' class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"> {{ $p->nombreProducto }} </h5>
+                    <p class="card-text"> Cantidad :  {{ $p->cantidadProducto }} </p>
+                    <p class="card-text"> Precio :  {{ $p->precioProducto }} </p>
+                    <p class="card-text"> Categoria :  {{ $p->nombreCategoria }} </p>
+                </div>
+            </div>
+        </div>
+    @endforeach
+    </div>
+</div>
 @stop
+
+
+
+        
