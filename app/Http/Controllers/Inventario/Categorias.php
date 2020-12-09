2830c4905b8d7b895031e2eac9f7c809878ca169
Marcela Guerrero <marcelaguerrero1396@gmail.com>
2020-12-09 12:10:17 -0500

    9-12-20

--- app/Http/Controllers/Inventario/Categorias.php
+++ app/Http/Controllers/Inventario/Categorias.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers\Inventario;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Categoria; 
 
 class Categorias extends Controller
 {
     public function index(){
         // Visualizar categorias
-        $categorias = DB::table('categorias')
-                    ->get();
+        $categorias = Categoria::all();
+
+        // Visualizar categorias cuya descripcion empiece por L
+        //$categorias = Categoria::where('descripcion', 'like', 'L%')->get();
         return view('categorias.listado',['categorias' => $categorias] );
     }
 
@@ -19,9 +22,14 @@ class Categorias extends Controller
         return view('categorias.form_registro') ;
     }
 
-    public function registrar()
+    public function registrar(Request $request)
     {
-        return view('categorias.form_registro');
+        //Registro de una categoria
+        $category = new Categoria();
+        $category->nombreCategoria = $request->input('nombreCat');
+        $category->descripcion  = $request->input('descripcionCat');
+        $category->save();
+        return redirect()->route('listadoCategorias');   
     }
 
     public function actualizar()
--- resources/views/categorias/form_registro.blade.php
+++ resources/views/categorias/form_registro.blade.php
@@ -1,4 +1,26 @@
 @extends('main')
 @section('contenido')
 
+<div class="container">
+    <h2> Registro Categoria </h2> <br> <br>
+    <form action ="{{url('categorias/registro')}}" method="POST" >
+        @csrf
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1">@</span>
+            </div>
+            <input type="text" class="form-control" placeholder="Nombre Categoria" aria-label="Username" aria-describedby="basic-addon1" id="nombreCat" name="nombreCat" required>
+        </div>
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="basic-addon1">@</span>
+            </div>
+            <input type="text" class="form-control" placeholder="Descripcion" aria-label="Username" aria-describedby="basic-addon1" id="descripcionCat" name="descripcionCat" required>
+        </div>
+
+        <button type="submit" class="btn btn-success">Registrar</button>
+    </form>
+
+</div>
+
 @stop
